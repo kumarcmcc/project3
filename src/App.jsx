5a4f2ac4d9e2a5e227876e2220171212bb63cd68
Chandan <kumarcmcc@gmail.com>
2023-12-18 11:19:07 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import First from './Components/first'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <First />
+          </>
+  )
+}
+
+export default App
---
+++ src/Components/First.jsx
@@ -0,0 +1,31 @@
+import './First.css'
+
+function First() {
+    return (
+        <>
+
+
+<div class="parts118-123ceh8iJkSkU_text_wrap">
+    <div class="parts118-123ceh8iJkSkU_text">
+        <h2 class="f_meiryo f_bold">
+            兵庫県神戸市に地域密着！左官工事を専門に行なう有限会社菊田組です！
+        </h2>
+        <p class="f_meiryo">
+            有限会社菊田組のホームページへようこそ！弊社は兵庫県神戸市を拠点に、地域密着で活動している左官屋です。左官工事一式に対応できます。<br />
+施工は業歴３０年の代表自らと、腕利きの職人たちが担当します。他社では難しい左官も、ぜひ私たちに任せてください！丁寧な作業で品質を追及いたします。<br />
+これからも菊田組は、地域の皆さまに愛される会社を目指し、お客さま一人ひとりに寄り添って、最高の仕上がりを提供してまいります。
+        </p>
+    </div>
+            <div class="parts118-123ceh8iJkSkU_link_wrap">
+        <a class="parts118-123ceh8iJkSkU_link f_meiryo f_bold" href="/information" >会社概要</a>
+    </div>
+        </div>
+
+
+
+            
+        </>
+    )
+}
+
+export default First
